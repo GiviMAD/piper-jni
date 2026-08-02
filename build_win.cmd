8a64f5ff163c4c8c6cc0d2c1207653c1142310d1
Florian Hotze <dev@florianhotze.com>
2026-08-02 02:27:30 +0200

    Resolve symlinks in native library resources

--- build_win.cmd
+++ build_win.cmd
@@ -1,3 +1,16 @@
-cmake -Bbuild -DCMAKE_INSTALL_PREFIX=src\main\resources\win-amd64
-cmake --build build --config Release
+set "TARGET_DIR=src\main\resources\win-%AARCH%"
+
+cmake -Bbuild -DCMAKE_INSTALL_PREFIX="%TARGET_DIR%"
+cmake --build build --config Release -j %NUMBER_OF_PROCESSORS%
 cmake --install build
+
+for /f "tokens=*" %%F in ('dir /b /s /a:L "%TARGET_DIR%\*.dll" 2^>nul') do (
+    set "LINK=%%F"
+    for /f "tokens=*" %%T in ('powershell -NoProfile -Command "(Get-Item '!LINK!').Target"') do set "TARGET=%%T"
+    if defined TARGET if exist "!TARGET!" (
+        for %%D in ("!TARGET!") do set "TARGET_PARENT=%%~dpD"
+        del /f /q "!LINK!"
+        move /y "!TARGET!" "!LINK!"
+        rd "!TARGET_PARENT!" 2>nul
+    )
+)
