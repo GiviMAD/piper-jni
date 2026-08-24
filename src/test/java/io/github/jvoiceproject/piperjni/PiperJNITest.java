17c245b817cf1414f674477e2bcd38d2aa8956be
Florian Hotze <dev@florianhotze.com>
2026-08-24 14:47:13 +0200

    Fix license headers

--- src/test/java/io/github/jvoiceproject/piperjni/PiperJNITest.java
+++ src/test/java/io/github/jvoiceproject/piperjni/PiperJNITest.java
@@ -2,7 +2,7 @@
  * #%L
  * piper-jni
  * %%
- * Copyright (C) 2023 - 2026 Contributors to whisper-jni
+ * Copyright (C) 2023 - 2026 Contributors to piper-jni
  * %%
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
